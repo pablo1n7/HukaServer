375130bd5e160599fd1a0eb6876149487c0bbf8e
Jose Pablo Navarro <pablo1n7@h248.53.82.166.dynamic.ip.windstream.net>
2022-06-30 13:09:24 -0300

    fix

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello World!"
+def paint():
+    return render_template('index.html')
+
+
+@app.route('/setEspecies', methods=['POST'])
+def especie():
+    print('hola')
