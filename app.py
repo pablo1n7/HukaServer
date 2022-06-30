37eae58d6052aeaf08d29c74b90e9ab9eeb33023
Jose Pablo Navarro <pablo1n7@h248.53.82.166.dynamic.ip.windstream.net>
2022-06-30 14:15:48 -0300

    app

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask import render_template, request, jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def paint():
+    try:
+        d = pd.read_csv('data.csv')
+    except:
+        return d.to_html()
     return render_template('index.html')
 
 
@@ -15,6 +20,8 @@ def especie():
     if request.method == "POST":
         json_dict = request.get_json()
         print(json_dict)
+        df = pd.DataFrame(json_dict)
+        df.to_csv('data.csv')
         data = {'resp': 'prueba',}
         return jsonify(data)
     else:
