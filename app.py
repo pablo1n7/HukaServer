3c81044ebc546e606b3d33df003901c2f88b2534
Jose Pablo Navarro <pablo1n7@h248.53.82.166.dynamic.ip.windstream.net>
2022-06-30 12:51:06 -0300

    error

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-from flask import Flask, url_for, render_template, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def paint():
-    return render_template('index.html')
-
-
-@app.route('/setEspecies', methods=['POST'])
-def especie():
-    print('hola')
-
-if __name__=='__main__':
-    app.run(debug=True)
+def index():
+    return "Hello World!"
